1fdb64b351227ee3b1ed95e17c010435b230af6d
Hanzel Legarda <hlegarda@gmail.com>
2025-12-29 14:40:05 -0600

    Update main.cpp

--- Arduino-DHT_Sensor/src/main.cpp
+++ Arduino-DHT_Sensor/src/main.cpp
@@ -1,18 +1,5 @@
 #include <Arduino.h>
-
-const int buttonPin = 2;
-
-void setup() {
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
-}
-
-void loop() {
-  int state = digitalRead(buttonPin);
-  Serial.println(state);
-  delay(200);
-}
-/* #include <DHT.h>
+#include <DHT.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -21,25 +8,24 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(DHTPIN, INPUT_PULLUP); // internal pull-up
+  pinMode(DHTPIN, INPUT_PULLUP);
   dht.begin();
 }
 
 void loop() {
-  delay(2000);
+  delay(4000);
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
+  if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read DHT11");
     return;
   }
 
-  Serial.print("Temp: ");
-  Serial.print(t);
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
   Serial.print(" °C | Hum: ");
-  Serial.print(h);
+  Serial.print(humidity);
   Serial.println(" %");
 }
- */
